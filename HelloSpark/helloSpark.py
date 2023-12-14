2dbdc96a041e2ee036f78f7a1ed3b6f1f72f0615
Saurabh Chaurasia <about.srb@gmail.com>
2023-12-14 12:50:29 +0530

    added spark session

--- HelloSpark/helloSpark.py
+++ HelloSpark/helloSpark.py
@@ -1,4 +1,10 @@
 from pyspark.sql import *
 
 if __name__ == "__main__" :
-    print("Hello")
+    spark = (SparkSession.builder \
+        .appName("Hello Spark")  \
+        .master("local[3]") \
+        # using local JVM cluter manager with 3 threads
+        .getOrCreate())
+
+    spark.stop()
