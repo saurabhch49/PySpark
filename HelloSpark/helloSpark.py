cdbe5bbd6c142fcb23c76f1a51151c4f5b643fdc
Saurabh Chaurasia <about.srb@gmail.com>
2023-12-14 16:44:33 +0530

    created Log4j class and tested

--- HelloSpark/helloSpark.py
+++ HelloSpark/helloSpark.py
@@ -1,10 +1,17 @@
 from pyspark.sql import *
+from lib.logger import Log4j
 
-if __name__ == "__main__" :
-    spark = (SparkSession.builder \
-        .appName("Hello Spark")  \
-        .master("local[3]") \
-        # using local JVM cluter manager with 3 threads
-        .getOrCreate())
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+             .appName("Hello Spark") \
+             .master("local[3]") \
+             .getOrCreate()
 
+
+    logger = Log4j(spark)
+    logger.info("Starting  HelloSpark")
+
+    logger.info("Finished  HelloSpark")
     spark.stop()
+
+
