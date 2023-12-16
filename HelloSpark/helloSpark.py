651e3e19ac60032b4cac0facd62ef9beb9de5658
Saurabh Chaurasia <about.srb@gmail.com>
2023-12-16 19:39:52 +0530

    reading csv file using DataFrame
    csv is an open source

--- HelloSpark/helloSpark.py
+++ HelloSpark/helloSpark.py
@@ -1,3 +1,5 @@
+import sys
+
 from pyspark.sql import *
 from lib.logger import Log4j
 from lib.utils import *
@@ -13,11 +15,22 @@ if __name__ == "__main__":
 
     logger = Log4j(spark)
 
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
     logger.info("Starting  HelloSpark")
     # Your processing code
-    conf_out = spark.sparkContext.getConf()
-    logger.info(conf_out.toDebugString())
-    logger.info("Finished  HelloSpark")
+    #conf_out = spark.sparkContext.getConf()
+    #logger.info(conf_out.toDebugString())
+    #logger.info("Finished  HelloSpark")
+
+    survey_df = spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv(sys.argv[1])
+    # set python parameter
+    survey_df.show()
     spark.stop()
 
 
